472589be6d885d34d0dd1e1a164e3a3743b45f4b
Owen Ripley <ojripley@gmail.com>
2019-09-24 13:54:26 -0400

    working version of timer.js completed. @ojripley was driving and @jomicm was navigating

--- timer.js
+++ timer.js
@@ -1,11 +1,9 @@
 const times = process.argv.slice(2);
 
 const alert = function(times) {
-
   for (let time of times) {
     setTimeout(() => {
       process.stdout.write('\x07');
-      console.log('sound!');
     }, time * 1000);
   }
 };
