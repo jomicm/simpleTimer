bd4fd9032a2b93f97e97678e8f760d225f379463
Owen Ripley <ojripley@gmail.com>
2019-09-24 14:04:19 -0400

    handled some exception cases (negative numbers, strings) for potential arguments. @ojripley was driving, @jomicm was navigating

--- timer.js
+++ timer.js
@@ -2,9 +2,11 @@ const times = process.argv.slice(2);
 
 const alert = function(times) {
   for (let time of times) {
-    setTimeout(() => {
-      process.stdout.write('\x07');
-    }, time * 1000);
+    if (typeof Number(time) === 'number' && Number(time) >= 0) {
+      setTimeout(() => {
+        process.stdout.write('\x07');
+      }, time * 1000);
+    }
   }
 };
 
